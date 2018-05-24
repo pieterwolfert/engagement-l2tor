b7ddf7a77a1eb9aa10ab8b96432b828834758b4d
Pieter Wolfert <pieter@pieterwolfert.com>
2018-05-24 11:41:06 +0200

    first example of emotion classifier net

--- script/emotionnetwork.py
+++ script/emotionnetwork.py
@@ -1,14 +1,40 @@
 import numpy as np
 import csv
 from preprocessing import Preprocessing
+import keras
 
 data_dir = "/home/pieter/data/emoreact/"
 
+def model():
+    model = Sequential()
+    # input: 100x100 images with 3 channels -> (100, 100, 3) tensors.
+    # this applies 32 convolution filters of size 3x3 each.
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(8, activation='sigmoid'))
+
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    return model
+
 def main():
     prep = Preprocessing(data_dir)
-    training_filenames = prep.getFileNames("train.txt")
-    training_labels = prep.getLabels("train.txt", len(training_filenames))
-    #todo resize image to 128x128 on the fly, pick one image per movieclip, train using the labels
+    x_train, y_train, x_test, y_test = prep.loadData("train.txt", "test.txt")
+    model = model()
+    model.fit(data, labels, epochs=20, batch_size=32)
+    score = model.evaluate(x_test, y_test, batch_size=32)
+    print(score)
 
 if __name__=="__main__":
     main()
--- script/preprocessing.py
+++ script/preprocessing.py
@@ -1,22 +1,48 @@
 import numpy as np
 import csv
+from scipy.misc import imread
+from scipy.misc import imresize
 
 class Preprocessing:
     def __init__(self, datadir):
         self.datadir = datadir
-        self.filenames = []
+
+    def loadData(self, train, test):
+        x_train_filenames = self.getFileNames(train)
+        x_test_filenames = self.getFileNames(test)
+        y_train = self.getLabels(train, len(x_train_filenames))
+        y_test = self.getLabels(test, len(x_test_filenames))
+        self.x_train, self.y_train = self.loadImages(x_train_filenames,\
+         y_train, "train/", (128,128))
+        self.x_test, self.y_test = self.loadImages(x_test_filenames,\
+            y_test, "test/", (128,128))
+        return self.x_train, self.y_train, self.x_test, self.y_test
 
     def getFileNames(self, filename):
+        temp = []
         with open(self.datadir + filename) as f:
             rdr = csv.reader(f)
             for row in rdr:
-                self.filenames.append(row[0][:-4])
-        return self.filenames
+                temp.append(row[0][:-4])
+        return temp
 
     def getLabels(self, filename, length):
-        self.labels = np.zeros(shape=(length, 8))
+        labels = np.zeros(shape=(length, 8))
         with open(self.datadir + filename) as f:
             rdr = csv.reader(f)
             for i, item in enumerate(rdr):
-                self.labels[i] = item[1:9]
-        return self.labels
+                labels[i] = item[1:9]
+        return labels
+
+    def loadImages(self, filenames, labels, folder, size):
+        ext = "/10.png"
+        x_train = []
+        y_train = []
+        for x, y in zip(filenames, labels):
+            try:
+                tmp = imread(self.datadir + folder + x + ext)
+                x_train.append(imresize(tmp, size, interp='bicubic'))
+                y_train.append(y)
+            except FileNotFoundError as f:
+                pass
+        return np.asarray(x_train), np.asarray(y_train)
